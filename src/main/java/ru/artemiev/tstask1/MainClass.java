651d9e9a3f8982fbbbc15eaf67fd4291d38a6931
AAARTEMYEV <aaartemyev@tsconsulting.ru>
2018-02-13 16:20:47 +0300

    Раскомментирован метод checkTransfer

--- src/main/java/ru/artemiev/tstask1/MainClass.java
+++ src/main/java/ru/artemiev/tstask1/MainClass.java
@@ -3,7 +3,7 @@ package ru.artemiev.tstask1;
 public class MainClass {
     public static void main(String[] args) {
         Company company = new Company();
-//        company.checkTransfer();
+        company.checkTransfer();
         company.writeFile(args);
     }
 }
